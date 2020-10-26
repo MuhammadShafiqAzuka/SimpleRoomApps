30580bc8e6f76103f2694003bd23f53a34f5ba98
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-26 11:51:28 +0800

    Create the view model (bind logic and data from repo)

---
+++ app/src/main/java/com/azuka/mainactivity/viewmodel/UserViewModel.kt
@@ -0,0 +1,30 @@
+package com.azuka.mainactivity.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.azuka.mainactivity.data.User
+import com.azuka.mainactivity.data.UserDatabase
+import com.azuka.mainactivity.repository.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application): AndroidViewModel(application) {
+
+    private val readAllData: LiveData<List<User>>
+    private val repository: UserRepository
+
+    init {
+        val userDao = UserDatabase.getDatabase(application).userDao()
+        repository = UserRepository(userDao)
+        readAllData = repository.readAllData
+    }
+
+    fun addUser(user: User){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.addUser(user)
+        }
+    }
+
+}
